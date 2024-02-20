b730973ffd0a8c40d03d2fed2daeaf963fc959f3
maxlee <pj4316@naver.com>
2024-02-20 20:42:40 +0900

    feat: init

---
+++ data.md
@@ -0,0 +1,13 @@
+# Example Markmap Data
+
+- Root
+  - Node 1
+    - Subnode 1.1
+      - Subsubnode 1.1.1
+      - Subsubnode 1.1.2
+    - Subnode 1.2
+  - Node 2
+    - Subnode 2.1
+      - Subsubnode 2.1.1
+    - Subnode 2.2
+  - Node 3
--- main.ts
+++ main.ts
@@ -1,73 +1,68 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, renderResults } from 'obsidian';
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface MarkmapToCsvSettings {
+	directory: string
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class MarkmapToCsvPlugin extends Plugin {
+	settings: MarkmapToCsvSettings;
 
 	async onload() {
 		await this.loadSettings();
 
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
+		const markMapToCsvIconE1 = this.addRibbonIcon('dice', 'Markmap to csv Plugin', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
 			new Notice('This is a notice!');
 		});
 		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		markMapToCsvIconE1.addClass('markmap2csv-plugin-ribbon-class');
 
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
 		const statusBarItemEl = this.addStatusBarItem();
 		statusBarItemEl.setText('Status Bar Text');
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+		console.log('loading MarkmapToCSVPlugin');
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+		this.addCommand({
+			id: 'convert-markmap-to-csv',
+			name: 'Convert Markmap to CSV',
+			hotkeys: [
+				{
+					modifiers: ['Ctrl', 'Shift', 'Alt'],
+					key: 'M'
+				}
+			],
+			checkCallback: (checking) => {
+				const activeView = this.app.workspace.getActiveViewOfType(MarkdownView);
+				if (activeView) {
+					const markdownData = activeView.editor.getValue();
+					const markMaps = this.getMarkmaps(markdownData)
+					if(markMaps != null) {
+						const markmaps = this.getMarkmaps(markdownData)
+					
+						const markMapCsvs = markmaps?.map((markmap) => {
+							return this.convertMarkmapToCSV(markmap);
+						})
+						
+						if(markMapCsvs) {
+							const csvData = markMapCsvs.filter((markMapCsv) => { return markMapCsv.trim() !== "" }).join('\n')
+							this.saveCsvToFile(this.app.workspace.getActiveFile()?.basename ?? "markMap", csvData);
+						}
+
+						return true;
+					} else {
+						new Notice('ERROR: Please open a Markmap file to convert.');
+						return false;
+					}
+				} else {
+					new Notice('ERROR: Please open a Markmap file to convert.');
+					return false;
 				}
 			}
 		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
+		
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
 		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
@@ -76,6 +71,8 @@ export default class MyPlugin extends Plugin {
 
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
 		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+
+		this.addSettingTab(new MarkMapToCsvSettingTab(this.app, this));
 	}
 
 	onunload() {
@@ -83,52 +80,112 @@ export default class MyPlugin extends Plugin {
 	}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign({}, await this.loadData(), {
+			directory: '.'
+		} as MarkmapToCsvSettings);
 	}
 
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	convertMarkmapToCSV(data: string): string {
+		console.log(data)
+		const markmapData = data.replace(/^---[\s\S]*?---\n/, '');
+		const lines: string[] = markmapData.trim().split('\n');
+		const csvRows: string[] = [];
+		let stack: string[] = [];
+		let currentDepth: number = 0;
+
+		for (const line of lines) {
+			if (line.startsWith('#')) {
+				// Header indicates a new depth level
+				const headerLevel: number = line.match(/^#+/)?.[0].length || 1;
+				if(headerLevel == 1) {
+					// clear stack all 
+					// start new mindmap
+					stack = []
+				}
+				if (stack.length-1 >= headerLevel) {
+					const newData = stack.join(',')
+					csvRows.push(newData)
+					stack = stack.slice(0, headerLevel)
+				}
+				stack.push(line.substring(headerLevel).trim()); // Add header to stack
+				currentDepth = headerLevel; // Update current depth
+			} else if (line.trimStart().startsWith('-')) {
+				// List item indicates a new data row
+				const indent: number = this.getIndentCount(line.split('-')[0]); // Calculate indent level
+				const depth: number = indent + 3; // Calculate depth
+				const item: string = line.substring(line.indexOf('-') + 1).trim(); // Extract item
+				if (stack.length-1 >= depth) {
+					const newData = stack.join(',')
+					csvRows.push(newData)
+					stack = stack.slice(0, depth)
+				}
+				stack.push(item); // Set item in stack
+				currentDepth = depth; // Update current depth
+			}
+		}
+
+		if(stack.length >0) {
+			const lastData = stack.join(',')
+			csvRows.push(lastData)
+			stack = []
+		}
+
+		return csvRows.join('\n');
+    }
+
+	getIndentCount(line: string): number {
+		const match = line.replace(/\t/g, '    ').match(/^[\s]*/);
+		return match ? match[0].length / 4 : 0;
+	}
+
+    saveCsvToFile(filename: string, csvData: string): void {
+		const directory = this.settings.directory ?? this.app.workspace.getActiveFile()?.path ?? '.'
+		const fullPath = `${directory}/markmap-${filename}-${this.getCurrentDateTimeString()}.csv`
+        this.app.vault.create(fullPath, csvData)
+    }
+
+	getMarkmaps(markdownData: string): string[] | null {
+		const pattern = /```markmap\n([\s\S]*?)```/g
+		const match = markdownData.match(pattern)
+		if(match) {
+			return match
+		} else return null
+	}
+
+	getCurrentDateTimeString() {
+		const now = new Date();
+		const year = now.getFullYear();
+		const month = String(now.getMonth() + 1).padStart(2, '0');
+		const day = String(now.getDate()).padStart(2, '0');
+		const hours = String(now.getHours()).padStart(2, '0');
+		const minutes = String(now.getMinutes()).padStart(2, '0');
+		const seconds = String(now.getSeconds()).padStart(2, '0');
+		return `${year}${month}${day}${hours}${minutes}${seconds}`;
+	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class MarkMapToCsvSettingTab extends PluginSettingTab {
+    constructor(app: App, private plugin: MarkmapToCsvPlugin) {
+        super(app, plugin);
+    }
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+    display(): void {
+        const { containerEl } = this;
+        const setting = new Setting(containerEl)
+            .setName('directory')
+            .setDesc('Configure save csv directory. (default= activateFile\'s path)')
+            .addText(text => {
+                text
+                    .setPlaceholder('input your directory')
+                    .setValue(this.plugin.settings.directory)
+                    .onChange(async (value) => {
+                        this.plugin.settings.directory = value;
+                        await this.plugin.saveSettings();
+                    });
+            });
+    }
 }
